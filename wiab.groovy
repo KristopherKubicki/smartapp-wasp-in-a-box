8776c2e9a1992f1838dc63aab3a66a1445569d8f
Kristopher Kubicki <kristopher@acm.org>
2015-02-09 21:30:36 -0600

    Add support for motion sensors

--- wiab.groovy
+++ wiab.groovy
@@ -3,13 +3,15 @@
  *    paired with simulated presence sensors.  This implementation was borrowed from 
  *  http://dreamgreenhouse.com/projects/2013/presence/index.php
  *
+ * Version 0.1: Initial commit
+ * Version 0.2: Adds support for motion perimeter sensors
  *
  */
  
 definition(
     name: "Wasp in a Box",
     namespace: "KristopherKubicki",
-    author: "Kristopher Kubicki",
+    author: "kristopher@acm.org",
     description: "Implements the 'Wasp in a Box' algorithm for simulated presence sensors",
     category: "Convenience",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
@@ -20,10 +22,12 @@ preferences {
 		input "occupy", "capability.presenceSensor", title: "Which?"
 	}
 	section("When there's movement..."){
-		input "motions", "capability.motionSensor", title: "Where?", multiple: true
+		input "imotions", "capability.motionSensor", title: "Where?", multiple: true, required:false
+        input "icontacts", "capability.contactSensor", title: "Where?", multiple: true, required:false
 	}
-    section("And then off when any of these perimeter contact sensors are opened..."){
-		input "contacts", "capability.contactSensor", title: "Where?", multiple: true
+    section("And then off when any of these perimeter contact sensors are opened or tripped..."){
+		input "ocontacts", "capability.contactSensor", title: "Where?", multiple: true, required: false
+        input "omotions", "capability.motionSensor", title: "Where?", multiple: true, required:false
 	}
 }
 
@@ -38,16 +42,23 @@ def updated() {
 }
 
 def initialize() {
-	subscribe(motions, "motion.active", motionHandler)
-    subscribe(contacts, "contact.open", contactHandler)
+	subscribe(icontacts, "contact.open", insideHandler)
+	subscribe(imotions, "motion.active", insideHandler)
+    subscribe(ocontacts, "contact.open", outsideHandler)
+    subscribe(omotions, "motion.active", outsideHandler)
 }
 
 
-def motionHandler(evt) {
+def insideHandler(evt) {
 	log.debug "$evt.name: $evt.value"
     def success = 1
-    for (sensor in settings.contacts) { 
+    for (sensor in settings.ocontacts) { 
        if(sensor.latestValue("contact") == "open") { 
+           success = 0
+       }
+    }
+    for (sensor in settings.omotions) { 
+       if(sensor.latestValue("motion") == "active") { 
            success = 0
        }
     }
@@ -56,7 +67,8 @@ def motionHandler(evt) {
     }
 }
 
-def contactHandler(evt) {
+def outsideHandler(evt) {
 	log.debug "$evt.name: $evt.value"
     occupy.departed()
 }
+
